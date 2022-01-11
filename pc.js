690449ae2b55f20400b3538b3edfbf7bc1cfed4d
RenanFiaux <95436036+RenanFiaux@users.noreply.github.com>
2022-01-11 10:52:15 -0300

    Create pc.js

---
+++ pc.js
@@ -0,0 +1,25 @@
+nhas (21 sloc)  470 Bytes
+   
+{
+  "name" : " servicesti " ,
+  "versão" : " 1.0.0 " ,
+  "description" : " Sistema de controle de uma empresa de serviços de TI " ,
+  "main" : " index.js " ,
+  "roteiros" : {
+    "test" : " echo \" Erro: nenhum teste especificado \ " && exit 1 "
+  },
+  "autor" : " Sara " ,
+  "licença" : " ISC " ,
+  "dependências" : {
+    "cors" : " ^2.8.5 " ,
+    "expresso" : " ^ 4.17.1 " ,
+    "mysql2" : " ^2.3.0 " ,
+    "sequelizar" : " ^6.6.5 "
+  },
+  "devDependencies" : {
+    "nodemon" : " ^2.0.12 " ,
+    "sequelize-cli" : " ^6.2.0 "
+  }
+}
+© 2022 GitHub, Inc.
+Termos
